d06035253ee908f630b08ae2566bcdee531b3191
Song-git-hub-crypto <ssarikun2@gmail.com>
2026-09-08 14:40:28 +0900

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -4,7 +4,7 @@ const SHEETS = {
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc6NHqgbvbXimo0lH1zWQpoz0Krbraq6OoEHqsr8iZiG24AYd-gZthQd7DR3OcqIFzAk1y_Z-wrZTG/pub?gid=0&single=true&output=csv",
 
   "TKSDKSTKSRLVLF":
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc6NHqgbvbXimo0lH1zWQpoz0Krbraq6OoEHqsr8iZiG24AYd-gZthQd7DR3OcqIFzAk1y_Z-wrZTG/pub?gid=115731328&single=true&output=csv",
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSxF5EBUjAOi6yA7Yd48uNGHJ8rJ9ZqM26LC3lP0ir9cUEXQ357THq7acub_2AmFLfOcfj5W4UNVfO9/pub?gid=0&single=true&output=csv",
 
   "FIRE02":
     "여기에_소방설비기사기계_CSV_URL",
