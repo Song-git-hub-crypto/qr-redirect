387978550c1cc4aa78f2169668eb32ac5117af43
Song-git-hub-crypto <ssarikun2@gmail.com>
2026-09-08 14:25:23 +0900

    Create sheets.js

---
+++ sheets.js
@@ -0,0 +1,18 @@
+const SHEETS = {
+
+  "TKSDKSRLVLF":
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc6NHqgbvbXimo0lH1zWQpoz0Krbraq6OoEHqsr8iZiG24AYd-gZthQd7DR3OcqIFzAk1y_Z-wrZTG/pub?gid=0&single=true&output=csv",
+
+  "TKSDKSTKSRLVLF":
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc6NHqgbvbXimo0lH1zWQpoz0Krbraq6OoEHqsr8iZiG24AYd-gZthQd7DR3OcqIFzAk1y_Z-wrZTG/pub?gid=115731328&single=true&output=csv",
+
+  "FIRE02":
+    "여기에_소방설비기사기계_CSV_URL",
+
+  "ELECTRIC01":
+    "여기에_전기기사_CSV_URL",
+
+  "FORK01":
+    "여기에_지게차_CSV_URL"
+
+};
